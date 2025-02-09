4e97edc4cd7a25fe38bd266178de4a8c351f2e69
minamirza <minamirzeva@gmail.com>
2025-02-09 22:48:28 +0400

    chess js

--- createBoard.js
+++ createBoard.js
@@ -1,3 +1,4 @@
+import blackPawns from "./img/horse.png";
 export const createBoard = () => {
   const vertical = ["8", "7", "6", "5", "4", "3", "2", "1"];
   const horizontal = ["a", "b", "c", "d", "e", "f", "g", "h"];
